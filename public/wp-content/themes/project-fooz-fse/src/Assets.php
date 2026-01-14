64c32b2fddf3cff087be3a784f39e703e9814a2c
Jakub Muda <jakubmuda@gmail.com>
2026-01-13 21:52:35 -0500

    Improve names

--- public/wp-content/themes/project-fooz-fse/src/Assets.php
+++ public/wp-content/themes/project-fooz-fse/src/Assets.php
@@ -11,31 +11,43 @@ class Assets
 
     public static function enqueue(): void
     {
-        self::enqueue_cdn_styles();
-        self::enqueue_local_styles();
-        self::enqueue_cdn_scripts();
-        self::enqueue_local_scripts();
+        self::styles_cdn();
+        self::styles_local();
+        self::scripts_cdn();
+        self::scripts_local();
     }
 
-    private static function enqueue_cdn_styles(): void
+    /**
+     * Styles from CDN
+     */
+    private static function styles_cdn(): void
     {
         // 
     }
 
-    private static function enqueue_local_styles(): void
+    /**
+     * Local styles
+     */
+    private static function styles_local(): void
     {
         wp_enqueue_style('main', get_stylesheet_directory_uri() . '/style.css', []);
     }
 
-    private static function enqueue_cdn_scripts(): void
+    /**
+     * Sripts from CDN
+     */
+    private static function scripts_cdn(): void
     {
         wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', [], false, true);
     }
 
-    private static function enqueue_local_scripts(): void
+    /**
+     * Local scripts
+     */
+    private static function scripts_local(): void
     {
         wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/js/scripts.js', ['bootstrap'], false, true);
-        
+
         wp_localize_script('main', 'foozAjax', [
             'ajaxUrl' => admin_url('admin-ajax.php'),
             'nonce'   => wp_create_nonce('fooz_books'),
