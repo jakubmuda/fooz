d761346343e3787e882de6343afe82b067344af3
Jakub Muda <jakubmuda@gmail.com>
2026-01-13 20:32:35 -0500

    Add custom taxonomy

---
+++ public/wp-content/themes/project-fooz-fse/src/Taxonomy/Genre.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Fooz\Taxonomy;
+
+use Fooz\PostType\Books;
+
+class Genre extends AbstractTaxonomy
+{
+    public const TAXONOMY = 'genre';
+
+    protected function get_object_types(): array
+    {
+        return [Books::POST_TYPE];
+    }
+
+    protected function get_args(): array
+    {
+        return [
+            'label'         => 'Kategorie',
+            'public'        => true,
+            'hierarchical'  => true,
+            'show_in_rest'  => true,
+            'rewrite'       => ['slug' => 'book-genre'],
+            'labels'        => [
+                'name'              => __('Genres', TEXT_DOMAIN_THEME),
+                'singular_name'     => __('Genre', TEXT_DOMAIN_THEME),
+                'search_items'      => __('Search Genres', TEXT_DOMAIN_THEME),
+                'all_items'         => __('All Genres', TEXT_DOMAIN_THEME),
+                'edit_item'         => __('Edit Genre', TEXT_DOMAIN_THEME),
+                'update_item'       => __('Update Genre', TEXT_DOMAIN_THEME),
+                'add_new_item'      => __('Add New Genre', TEXT_DOMAIN_THEME),
+                'new_item_name'     => __('New Genre Name', TEXT_DOMAIN_THEME),
+                'menu_name'         => __('Genre', TEXT_DOMAIN_THEME),
+            ],
+        ];
+    }
+}
